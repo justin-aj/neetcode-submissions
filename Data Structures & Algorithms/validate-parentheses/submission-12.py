45b971b47b4eddba2865b2f5b454a3e6e334c3b0
Ajin Frank Justin <ajinfrankj@gmail.com>
2026-06-25 21:53:46 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {"}": "{", ")": "(", "]": "["}
+        stack = deque()
+        for bracket in s:
+            if bracket in dic:
+                if stack and stack[-1] == dic[bracket]:
+                    print(bracket)
+                    stack.pop()
+                else:
+                    return False
+            else: stack.append(bracket)
+    
+        return False if stack else True
+
+
